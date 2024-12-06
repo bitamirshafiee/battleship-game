1a01783438e0e55fc4fec5454141ee771357b171
mirshafiee <b.mirshafiee90@gmail.com>
2024-12-06 22:32:42 +0100

    Add playerA and position ships fun

--- App.tsx
+++ App.tsx
@@ -1,118 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import {useEffect, useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  PlayerA,
+  ShipName,
+  ShipsAndOpponentsShot,
+} from './players/playera/PlayerA';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): React.JSX.Element {
+  const [list, setList1] = useState<ShipsAndOpponentsShot[]>(new Array(100));
+  useEffect(() => {
+    const playerA = new PlayerA();
+    playerA.positionShips();
+    setList1(playerA.shipsAndOpponentsShotList);
+  }, []);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <FlatList
+      data={list}
+      numColumns={10}
+      renderItem={({item}) => (
+        <View>
+          <Text style={cellStyle(item ? item.ship.name : '', 20)}>
+            {item ? item.ship.name : ''}
+          </Text>
+        </View>
+      )}
+    />
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+function cellStyle(name: string, cellSize: number) {
+  const backgroundColor = getShipColorByName(name);
+  return {
+    height: cellSize,
+    width: cellSize,
+    backgroundColor: backgroundColor,
+    borderWidth: 1,
   };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  cell: {
+    height: 45,
+    width: 45,
+    borderWidth: 1,
   },
 });
-
 export default App;
+
+export function getShipColorByName(name: string): string {
+  switch (name) {
+    case ShipName.Carrier:
+      return 'green';
+    case ShipName.Battleship:
+      return 'orange';
+    case ShipName.Cruiser:
+      return 'yellow';
+    case ShipName.Submarine:
+      return 'blue';
+    case ShipName.Destroyer:
+      return 'pink';
+  }
+  return '';
+}
--- players/playera/PlayerA.ts
+++ players/playera/PlayerA.ts
@@ -1,16 +1,91 @@
+import {getRandomNumber} from '../../utils/Helper';
+
 export class PlayerA {
+  public shipsAndOpponentsShotList: ShipsAndOpponentsShot[] = new Array(100);
+  public positionShips() {
+    ships.forEach(ship => {
+      this.arrangeShipHorizontally(ship);
+      // const shipDirectionHorizontalOrVertical = getRandomNumber(
+      //   ShipDirection.Horizontal,
+      //   ShipDirection.Vertical,
+      // );
+      // if (shipDirectionHorizontalOrVertical == ShipDirection.Horizontal) {
+      //   this.arrangeShipHorizontally(ship);
+      // } else if (shipDirectionHorizontalOrVertical == ShipDirection.Vertical) {
+      // }
+    });
+  }
 
-    constructor(private list[])
+  public arrangeShipHorizontally(ship: Ship) {
+    const shipHead = getRandomNumber(1, 100);
+    const shipTail = shipHead + ship.length;
+
+    if (shipTail <= 100) {
+      if (this.isCellOccupied(shipHead, shipTail)) {
+        if (this.isHeadAndTailOnTheSameRow(shipHead, shipTail)) {
+          this.shipsAndOpponentsShotList?.fill(
+            {ship: ship, opponentShot: ''},
+            shipHead - 1,
+            shipTail - 1,
+          );
+        } else this.arrangeShipHorizontally(ship);
+      } else {
+        this.arrangeShipHorizontally(ship);
+      }
+    } else {
+      this.arrangeShipHorizontally(ship);
+    }
+  }
+
+  isCellOccupied(head: number, tail: number): boolean {
+    const desiredCellsToBeOccupied = this.shipsAndOpponentsShotList.slice(
+      head - 1,
+      tail - 1,
+    );
+    console.log(desiredCellsToBeOccupied);
+
+    const result = desiredCellsToBeOccupied.every(v => {
+      v.ship.name.length == 0;
+    });
+    console.log({
+      every: result,
+    });
+    return result;
+  }
+
+  public isHeadAndTailOnTheSameRow(head: number, tail: number) {
+    const d = Math.floor((head - 1) / 10);
+    if (tail - 1 <= (d + 1) * 10) return true;
+    else return false;
+  }
+
+  public isBetween(min: number, max: number, value: number) {
+    if (value >= min && value <= max) return true;
+    else return false;
+  }
 }
 
-enum ShipName{
-    Carrier = 1,
-    Battleship = 2,
-    Cruiser = 3,
-    Submarine = 4,
-    Destroyer = 5
+export enum ShipName {
+  Carrier = 'Carrier',
+  Battleship = 'Battleship',
+  Cruiser = 'Cruiser',
+  Submarine = 'Submarine',
+  Destroyer = 'Destroyer',
 }
 
-export type ship  = {name : ShipName, length : number}
+export type Ship = {name: string; length: number};
 
-const ships = [{name : ShipName.Carrier, length : 5}, {name : ShipName.Battleship, length : 4}, {name : ShipName.Battleship, length : 4} ]
+const ships = [
+  {name: ShipName.Carrier, length: 5},
+  {name: ShipName.Battleship, length: 4},
+  {name: ShipName.Cruiser, length: 3},
+  {name: ShipName.Submarine, length: 3},
+  {name: ShipName.Destroyer, length: 3},
+];
+
+enum ShipDirection {
+  Horizontal = 1,
+  Vertical = 2,
+}
+
+export type ShipsAndOpponentsShot = {ship: Ship; opponentShot: ''};
